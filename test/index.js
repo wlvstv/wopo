9cb92e51e9d59b947686a7c0713b45c3360a372f
Shope <MichaelGShope@Gmail.com>
2018-02-10 16:19:36 -0500

    initial commit

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import wlvsbot from '../lib';
+
+describe('wlvsbot', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
