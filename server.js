a892cce3c1cf424609ec6c34d7c2ac951aec8126
Ben Kaiser <ben.kaiser12@gmail.com>
2018-02-21 21:47:36 -0700

    added routing to get command information when list item is clicked

--- server.js
+++ server.js
@@ -1,45 +1,59 @@
 // Dependencies
-var express         = require('express'),
-app                 = express(),
-bodyParser          = require('body-parser'),
-pgp                 = require('pg-promise')();
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  pgp = require("pg-promise")();
 
 // Configuration
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/lib/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/lib/public"));
 
-// db configuration
+// db config
 var connection = {
-    user: 'wopo',
-    host: 'wlvsdb.cy36wqif1j5l.us-east-1.rds.amazonaws.com',
-    database: 'wlvs',
-    password: "I'mgoingbacktobed",
-    port: 5432
-}
+  user: "wopo",
+  host: "wlvsdb.cy36wqif1j5l.us-east-1.rds.amazonaws.com",
+  database: "wlvs",
+  password: "I'mgoingbacktobed",
+  port: 5432
+};
 var db = pgp(connection);
 
-
-// Routes
-// root route
-app.get("/", function(req, res) {
-    
+// Webpage routes
+app.get("/wopo", function(req, res) {
+  db
+    .any("SELECT * FROM public.commands")
+    .then(commands => {
+      res.render("index", { commands: commands });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-// commands show route
-app.get("/commands", function(req, res) {
-    db
-        .any('SELECT * FROM public.commands')
-        .then(data => {
-            res.render("index", {commands: data});
-        })
-        .catch(error => {
-            console.log(error);
-        });    
+// API Routes
+var router = express.Router();
+
+router.route("/commands/:command").get(function(req, res) {
+  var reqCommand = req.params.command;
+  console.log(reqCommand);
+  db
+    .one("SELECT * FROM public.commands WHERE command = $1", reqCommand)
+    .then(commandData => {
+      console.log(commandData);
+      res.json(commandData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
+// register routes
+app.use("/wopo/api", router);
 
 // Start the web server
 var port = 3000 || process.env.PORT;
-app.listen(port, function(){
-    console.log("Server started on port " + port)
-})
+app.listen(port, function() {
+  console.log("Server started on port " + port);
+});
