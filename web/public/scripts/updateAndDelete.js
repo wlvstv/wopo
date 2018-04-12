087411a8e01456a37403f44a9bf6886330fb8d32
Ben Kaiser <benkaiser12>
2018-04-11 18:08:44 -0600

    updated logging messages

--- web/public/scripts/updateAndDelete.js
+++ web/public/scripts/updateAndDelete.js
@@ -1,38 +1,38 @@
 $(document).ready(() => {
   //#region update click
-  $("#btnUpdate").on("click", () => {
+  $('#btnUpdate').on('click', () => {
     // get command to update
-    var command = $("li.command.active")
+    var command = $('li.command.active')
       .text()
       .trim();
 
     // build update URL
-    var updateURL = "/wopo/api/commands/update/" + command;
+    var updateURL = '/wopo/api/commands/update/' + command;
 
     // build updated Comamnd Object
     var updatedCommand = {
-      newCommand: $("#command")
+      newCommand: $('#command')
         .val()
         .toString()
         .trim(),
-      newResponse: $("#response")
+      newResponse: $('#response')
         .val()
         .toString()
         .trim()
     };
 
     // Log info in console
-    console.log("=========================================");
+    console.log('=========================================');
     console.log(`Command: ${command}`);
     console.log(`API Update Route: ${updateURL}`);
     console.log(
       `the updated command contains: ${JSON.stringify(updatedCommand)}`
     );
-    console.log("=========================================");
+    console.log('=========================================');
 
     // Make update request
     $.ajax({
-      type: "POST",
+      type: 'POST',
       url: updateURL,
       data: updatedCommand,
       success: () => {
@@ -44,26 +44,24 @@ $(document).ready(() => {
   //#endregion
 
   //#region delete click
-  $("#btnDelete").on("click", () => {
+  $('#btnDelete').on('click', () => {
     // command to be deleted
-    var command = $("li.command.active")
+    var command = $('li.command.active')
       .text()
       .trim();
     console.log(`The command to be deleted: ${command}`);
 
     // Build delete URL
-    var deleteURL = "/wopo/api/delete/" + command;
+    var deleteURL = '/wopo/api/delete/' + command;
     console.log(`the delete url is  ${deleteURL}`);
 
     // Make delete request
     $.ajax({
-      type: "DELETE",
+      type: 'DELETE',
       url: deleteURL,
       success: () => {
         location.reload();
-        console.log(
-          `the command ${command} was deleted succesfully! ${result}`
-        );
+        console.log(`the command ${command} was deleted succesfully!`);
       },
       fail: () => {
         location.reload();
--- web/routes/apiroutes.js
+++ web/routes/apiroutes.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var connection = require("../../lib/db");
-var pgp = require("pg-promise")();
+var express = require('express');
+var connection = require('../../lib/db');
+var pgp = require('pg-promise')();
 var router = express.Router();
 
 var db = pgp(connection);
@@ -8,12 +8,12 @@ var db = pgp(connection);
 // API Routes
 //#region commands/update/:command Route
 router
-  .route("/commands/update/:command")
+  .route('/commands/update/:command')
   .get(function(req, res) {
     var reqCommand = req.params.command;
     console.log(reqCommand);
     db
-      .one("SELECT * FROM public.commands WHERE command = $1", reqCommand)
+      .one('SELECT * FROM public.commands WHERE command = $1', reqCommand)
       .then(commandData => {
         // console.log(commandData);
         res.json(commandData);
@@ -30,11 +30,11 @@ router
     );
     db
       .one(
-        "UPDATE public.commands SET command = $1, response = $2 WHERE command = $3",
+        'UPDATE public.commands SET command = $1, response = $2 WHERE command = $3',
         [updates.newCommand, updates.newResponse, changeCommand]
       )
       .then(() => {
-        res.redirect("/wopo");
+        res.redirect('/wopo');
       })
       .catch(error => {
         console.log(error);
@@ -46,11 +46,11 @@ router
 //#endregion
 
 //#region /commands/new routes
-router.route("/commands/new").post((req, res) => {
+router.route('/commands/new').post((req, res) => {
   var newCommand = req.body;
   db
     .none(
-      "INSERT INTO public.commands(command, isMod, response, creator, channel) VALUES ($1, $2, $3, $4, $5)",
+      'INSERT INTO public.commands(command, isMod, response, creator, channel) VALUES ($1, $2, $3, $4, $5)',
       [
         newCommand.command,
         newCommand.isMod,
@@ -60,7 +60,7 @@ router.route("/commands/new").post((req, res) => {
       ]
     )
     .then(() => {
-      res.redirect("/wopo");
+      res.redirect('/wopo');
     })
     .catch(error => {
       console.log(`There was an error: ${error}`);
@@ -69,12 +69,12 @@ router.route("/commands/new").post((req, res) => {
 //#endregion
 
 //#region delete route
-router.route("/delete/:command").delete((req, res) => {
+router.route('/delete/:command').delete((req, res) => {
   var command = req.params.command;
   console.log(`Command to be deleted: ${command}`);
   db
-    .any("DELETE FROM public.commands WHERE command = $1", command)
-    .then((data) => {
+    .any('DELETE FROM public.commands WHERE command = $1', command)
+    .then(data => {
       res.json(data);
     })
     .catch(error => {
